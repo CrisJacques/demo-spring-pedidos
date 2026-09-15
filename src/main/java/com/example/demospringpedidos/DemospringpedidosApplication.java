a04265cb7479099cbe918d519990b6fba650b300
Cristhiane Jacques <cjn_1707@hotmail.com>
2026-09-15 20:52:38 -0300

    initialize Spring Boot application with Maven setup and basic configuration

---
+++ src/main/java/com/example/demospringpedidos/DemospringpedidosApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospringpedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringpedidosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemospringpedidosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demospringpedidos/DemospringpedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospringpedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemospringpedidosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
